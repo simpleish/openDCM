5c420ac1dc77bc2e6394dafc5639904156aea67d
rakshitraj <rakshit3181@gmail.com>
2025-08-21 03:21:27 +0530

    add test stuff

---
+++ tests/liquibase_testing.py
@@ -0,0 +1,22 @@
+import asyncio
+from pyliquibase import Pyliquibase
+
+
+def perform_liquibase_testing():
+    try:
+        liquibase = Pyliquibase(defaultsFile="tests/liquibase.properties",
+                                jdbcDriversDir='external-jar',
+                                logLevel='INFO')
+        # Call execute with arguments
+        liquibase.execute("status")
+        # liquibase.execute("rollback", "MyTag")
+        liquibase.validate()
+        liquibase.status()
+        liquibase.updateSQL()
+        liquibase.update()
+                
+    except Exception as e:
+        exit(e)
+
+if __name__ == '__main__':
+    perform_liquibase_testing()
