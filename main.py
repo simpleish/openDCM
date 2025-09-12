c989545efd8f955cb16a5bf70c5f61b0d61599e9
rakshitraj <rakshit3181@gmail.com>
2025-09-13 00:07:51 +0530

    fix rollback argument

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def argument_parser():
 
     parser.add_argument("--dbname", help="Database to which DCM is applied")
     parser.add_argument("--migration_schema", help="Schema to which liquibase DCM is to be applied")
-    parser.add_argument("rollback_opt", help="yes/no on the rollback action")
+    parser.add_argument("--rollback_opt", help="yes/no on the rollback action")
     parser.add_argument("--tag", help="tag used for rollback and changeset")
 
     args = parser.parse_args()
